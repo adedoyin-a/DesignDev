f24f008cd17e734b75238c96f41965dc281fc0b0
Adedoyin Adebayo <dazzlemediaorg@gmail.com>
2022-10-13 14:25:46 +0100

    Eleventh commit

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ function toggleHamburger (){
     if(icon.classList.contains("fa-bars")){
         icon.classList.replace("fa-bars", "fa-xmark");
         document.getElementById("navbar").style.backgroundColor="#131B1A";
-        document.getElementById("navbar").style.height="20%";
+        document.getElementById("navbar").style.height="190px";
         document.querySelector("ul").style.display = "block";
     }
     else{
         icon.classList.replace("fa-xmark", "fa-bars");
         document.getElementById("navbar").style.backgroundColor="#070600";
-        document.getElementById("navbar").style.height="9%";
+        document.getElementById("navbar").style.height="80px";
         document.querySelector("ul").style.display = "none";
     }
 };
