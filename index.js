46ded6f910de203b21d4a5bef422e86563f695e5
Adedoyin Adebayo <110562771+adedoyin-a@users.noreply.github.com>
2023-06-14 01:50:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+
 let icon = document.querySelector("#hamburger");
 let modal = document.querySelector(".modal");
 let overlay = document.querySelector(".modal-overlay");
-// let openModal = document.querySelectorAll(".popup");
+let openModal = document.querySelectorAll(".popup");
 let closeModal = document.querySelector("#close-modal");
 const body = document.querySelector("body");
 // let modal_btn = document.querySelector("contact-btn"); 
@@ -24,24 +25,23 @@ function toggleHamburger (){
         document.querySelector("ul").style.display = "none";
     }
 };
-for (var i = 0; i < document.querySelectorAll(".popup").length; i++){
-document.querySelectorAll(".popup")[i].addEventListener("click","popUp");
 
-function popUp (){
-
+for (let i = 0; i < openModal.length; i++) {
+    openModal[i].addEventListener("click", () => {
     modal.classList.remove("hidden");
-
     overlay.classList.remove("hidden");
-
     body.style.overflow = "hidden";
-
     event.preventDefault()
-
-};
+    })
 }
+// openModal.addEventListener("click", popUp);
 
-
-
+// function popUp (){
+//     modal.classList.remove("hidden");
+//     overlay.classList.remove("hidden");
+//     body.style.overflow = "hidden";
+//     event.preventDefault()
+// };
 
 // modal_btn.addEventListener("click", function (){
 //     window.open("mailto:adedoyin.designdev@gmail.com");
@@ -55,3 +55,11 @@ function closePopUp (){
     overlay.classList.add("hidden");
     body.style.overflow = "auto";
 }
+
+
+
+
+
+
+
+
